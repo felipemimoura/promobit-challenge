6bacf2f0c85b8e35ba034df8f4b58545418ca35f
Felipe Moura <felipe.moura@interprocess.com.br>
2021-11-16 19:47:58 -0300

    chore: adicionado novas urls base para  aplicação

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,14 @@ const API_URL = 'https://api.themoviedb.org/3/'
 const API_KEY = process.env.REACT_APP_API_KEY
 
 const POPULAR_URL = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US`
+const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
+const BACKDROP_SIZE = 'w1280';
+
+
 
 export {
     API_URL,
-    POPULAR_URL
+    POPULAR_URL,
+    IMAGE_BASE_URL,
+    BACKDROP_SIZE
 }
