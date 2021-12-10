0d2a5a3efbc0b3d931bf8ce778b113a3fcf7e4c1
Felipe Moura <felipe.moura@interprocess.com.br>
2021-12-10 11:56:40 -0300

    feat: adiciona link de navegação para o filme escolhido

--- src/components/Thumb/index.tsx
+++ src/components/Thumb/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { BACKDROP_SIZE, IMAGE_BASE_URL } from '../../config'
 import { ThumbProps } from './interface'
 import * as Styled from './styles'
@@ -6,9 +7,9 @@ import * as Styled from './styles'
 export const Thumb: React.FC<ThumbProps> = ({ imageUrl, movieId, movieTitle }) => {
     const urlImage = `${IMAGE_BASE_URL}/${BACKDROP_SIZE}${imageUrl}`
     return (
-        <div>
+        <Link to={`/${movieId}`}>
             <Styled.Image src={urlImage} alt={movieTitle} />
-        </div>
+        </Link>
     )
 }
 
