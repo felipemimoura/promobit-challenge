929ff55e4952411f1411eb88f78eeb46719afbbf
Felipe Moura <felipe.moura@interprocess.com.br>
2021-12-10 14:42:58 -0300

    refactor: Adiciona o número total de páginas para realizar a chamada na api

--- src/services/controllers/getMovies/index.tsx
+++ src/services/controllers/getMovies/index.tsx
@@ -2,8 +2,8 @@ import { POPULAR_URL } from '../../../config'
 import api from '../../API'
 import { MovieContext } from './interface'
 
-export const getPopularMovies = async (): Promise<MovieContext> => {
-    const result = await api.get<MovieContext>(`${POPULAR_URL}`)
-    const { page, results } = result.data
-    return { page, results }
+export const getPopularMovies = async (pageNumber: number): Promise<MovieContext> => {
+    const result = await api.get<MovieContext>(`${POPULAR_URL}&page=${pageNumber}`)
+    const { page, results, total_pages } = result.data
+    return { page, results, total_pages }
 }
