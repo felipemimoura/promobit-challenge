0fea3d7ada9b49433f2bcba1e2561f344d84c51a
Felipe Moura <felipe.moura@interprocess.com.br>
2021-12-10 11:56:09 -0300

    refactor: adicoina link de navegação no header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { ReactComponent as TMDBLOGO } from '../../images/tmdb_logo.svg'
 import SvgIcon from '../SVGIcon'
 import { HeaderProps } from './interface'
@@ -7,7 +8,9 @@ import * as Styled from './styles'
 export const Header: React.FC<HeaderProps> = ({ userName }) => (
     <Styled.Header>
         <Styled.Content>
-            <SvgIcon Icon={TMDBLOGO} width={16} height={8} color="#FFf" />
+            <Link to="/" >
+                <SvgIcon Icon={TMDBLOGO} width={16} height={8} color="#FFf" />
+            </Link>
 
             <h3>{userName}</h3>
 
