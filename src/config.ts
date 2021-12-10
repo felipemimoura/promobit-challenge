deb0c3777fc410e8907711685e2261923d6fd298
Felipe Moura <felipe.moura@interprocess.com.br>
2021-12-10 18:01:56 -0300

    refactor: ajuste no backdrop size

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 const API_URL = 'https://api.themoviedb.org/3/'
 const API_KEY = process.env.REACT_APP_API_KEY
 
-const POPULAR_URL = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US`
+const POPULAR_URL = `${API_URL}movie/popular?api_key=${API_KEY}&language=pt-BR`
 const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p';
-const BACKDROP_SIZE = 'w1280';
+const BACKDROP_SIZE = '/w1280';
 
 const CATEGORIES_URL = `${API_URL}/genre/movie/list?api_key=${API_KEY}&language=pt-BR`
 
