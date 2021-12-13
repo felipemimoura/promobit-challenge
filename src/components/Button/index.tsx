095bd80b71da75881da2e9cee8e523309cdd1be4
Felipe Moura <felipe.moura@interprocess.com.br>
2021-12-13 09:22:41 -0300

    refactor: Ajuste no botão de carregar mais filmes

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,15 +1,13 @@
-import React from 'react'
-import { useMovies } from '../../contexts/getMovies'
-import { ButtonProps } from './interface'
-import * as Styled from './styles'
+import React from 'react';
+import { useMovies } from '../../contexts/getMovies';
+import { ButtonProps } from './interface';
+import * as Styled from './styles';
 
 export const Button: React.FC<ButtonProps> = ({ text }) => {
-    const { pages, loadMorePages } = useMovies()
+    const { pages, loadMorePages } = useMovies();
     return (
         <Styled.Button type="button" onClick={() => loadMorePages(pages + 1)}>
             {text}
         </Styled.Button>
-    )
-}
-
-
+    );
+};
